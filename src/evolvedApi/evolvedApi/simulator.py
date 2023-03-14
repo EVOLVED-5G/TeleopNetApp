0cd567642dfba1fa982985a5f80b3ce51b739c88
thomaspeyrucain <thomas.peyrucain@pal-robotics.com>
2023-03-14 14:09:03 +0100

    Teleop Network app V4.0 to work with CAPIF v3.0

--- src/evolvedApi/evolvedApi/simulator.py
+++ src/evolvedApi/evolvedApi/simulator.py
@@ -13,19 +13,12 @@ def get_token_for_nef_emulator() -> Token:
     configuration = swagger_client.Configuration()
     # The host of the 5G API (emulator)
     configuration.host = get_host_of_the_nef_emulator()
+    configuration.verify_ssl = False
     api_client = swagger_client.ApiClient(configuration=configuration)
     api_client.select_header_content_type(["application/x-www-form-urlencoded"])
     api = LoginApi(api_client)
     token = api.login_access_token_api_v1_login_access_token_post("", username, password, "", "", "")
     return token
-
-
-def get_api_client(token) -> swagger_client.ApiClient:
-    configuration = swagger_client.Configuration()
-    configuration.host = get_host_of_the_nef_emulator()
-    configuration.access_token = token.access_token
-    api_client = swagger_client.ApiClient(configuration=configuration)
-    return api_client
 
 def read_qos() -> int:
     response = requests.get(url=os.environ.get('ENDPOINT_ADDRESS'),
