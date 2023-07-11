3623e9fb69c7a84e64958cf04e3ea3394b5af4bc
thomaspeyrucain <thomas.peyrucain@pal-robotics.com>
2023-07-11 14:58:48 +0200

    Add TSN script

--- package_build.sh
+++ package_build.sh
@@ -3,6 +3,8 @@ set -e
 ./prepare.sh&
 # setup ros environment
 . /app/devel/setup.bash
+python3 tsn_sub.py
+sleep 120
 python3 -m flask run --host=0.0.0.0&
 export ROS_MASTER_URI=$ROBOT_ROS_MASTER_URI
 export ROS_IP=$PC_ROS_IP
