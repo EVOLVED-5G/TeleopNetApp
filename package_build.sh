4a6c56e637cad0fa35fefd41dfe995866ce0cf0f
thomaspeyrucain <thomas.peyrucain@pal-robotics.com>
2023-07-12 16:20:26 +0200

    Temporaly remove tsn

--- package_build.sh
+++ package_build.sh
@@ -2,8 +2,8 @@
 set -e
 # setup ros environment
 . /app/devel/setup.bash
-python3 tsn_sub.py
-sleep 120
+# python3 tsn_sub.py
+# sleep 120
 ./prepare.sh&
 python3 -m flask run --host=0.0.0.0&
 export ROS_MASTER_URI=$ROBOT_ROS_MASTER_URI
